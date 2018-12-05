a0e994b8c56edfeafdfbdc77f0be0bda02773a4f
Keith Carter <kcarter241@gmail.com>
2018-12-05 13:13:44 -0500

    Update cap deployment files

--- config/deploy/nu-test.rb
+++ config/deploy/nu-test.rb
@@ -1,4 +1,4 @@
-set :bundle_flags,  '--with postgres'
+set :bundle_flags,  '--with mysql'
 set :bundle_without, 'test production'
 set :rails_env, 'development'
-server 'avalon-web-dev.library.northwestern.edu', roles: %w{web app resque_worker resque_scheduler}, user: 'deploy'
+server '192.168.1.10', roles: %w{web app resque_worker resque_scheduler}, user: 'vagrant'
